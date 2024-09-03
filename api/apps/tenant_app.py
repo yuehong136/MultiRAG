75ff72204e08b9932a7ada283393a0973b1d8db2
yuehong136 <1365590480@qq.com>
2024-09-03 09:28:13 +0800

    feat-add api of tenant app

---
+++ api/apps/tenant_app.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+"""
+@project: multirag
+@Author：龙
+@file： tenant_app.py
+@date：2024/9/13 9:19
+@desc:
+"""
+
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from api.utils.api_utils import server_error_response
+from api.apps import manager
+from api.db.database import get_db
+from api.utils.api_utils import get_json_result, server_error_response, validate_request, get_data_error_result
+
+from api.db.services.user_service import TenantService, UserTenantService
+from api.utils.api_utils import get_json_result
+
+router = APIRouter()
+
+
+@router.get("/list", summary="获取租户列表", response_model=dict)
+async def tenant_list(db: Session = Depends(get_db), user=Depends(manager)):
+    try:
+        tenants = TenantService.get_by_user_id(db, user.id)
+        return get_json_result(data=tenants)
+    except Exception as e:
+        return server_error_response(e)
+
+
+@router.get("/<tenant_id>/user/list", summary="获取租户下用户列表", response_model=dict)
+async def user_list(tenant_id):
+    try:
+        users = UserTenantService.get_by_tenant_id(db, tenant_id)
+        return get_json_result(data=users)
+    except Exception as e:
+        return server_error_response(e)
