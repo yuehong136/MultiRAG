0b499e5a202b9b7d22a2d0a3d4f00d0c2b51456d
yuehong136 <1365590480@qq.com>
2024-09-27 09:17:02 +0800

    make it lighten

--- core/llm/embedding_model/youdao_embedding.py
+++ core/llm/embedding_model/youdao_embedding.py
@@ -1,7 +1,8 @@
 import os
 import numpy as np
 from typing import List, Optional
-from BCEmbedding import EmbeddingModel as qanthing
+
+from api.settings import LIGHTEN
 from core.llm.embedding_model.base import Base
 from api.utils.file_utils import get_home_cache_dir
 from core.utils import num_tokens_from_string
@@ -15,7 +16,8 @@ class YoudaoEmbed(Base):
         self.base_url = base_url
         self.kwargs = kwargs
         model_path = self.get_model_path(self.model_name.split("/")[-1] if "/" in self.model_name else self.model_name)
-        if not YoudaoEmbed._client:
+        if not LIGHTEN and not YoudaoEmbed._client:
+            from BCEmbedding import EmbeddingModel as qanthing
             try:
                 print(f"LOADING BCE from {model_path}...")
                 YoudaoEmbed._client = qanthing(model_name_or_path=model_path, **self.kwargs)
--- core/llm/rerank_model/youdao_rerank.py
+++ core/llm/rerank_model/youdao_rerank.py
@@ -1,9 +1,9 @@
 import os
-import re
 import threading
 
 import numpy as np
 
+from api.settings import LIGHTEN
 from api.utils.file_utils import get_home_cache_dir
 from core.llm.rerank_model.base import sigmoid
 from core.llm.rerank_model.default_rerank import DefaultRerank
@@ -18,8 +18,8 @@ class YoudaoRerank(DefaultRerank):
         self.model_name = model_name  # 修复：显式定义 model_name
         model_path = self.get_model_path(self.model_name.split("/")[-1] if "/" in self.model_name else self.model_name)
 
-        from BCEmbedding import RerankerModel
-        if not YoudaoRerank._model:
+        if not LIGHTEN and not YoudaoRerank._model:
+            from BCEmbedding import RerankerModel
             with YoudaoRerank._model_lock:
                 if not YoudaoRerank._model:
                     try:
