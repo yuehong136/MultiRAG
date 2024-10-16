a071f8a00fa9e178c1a46eb1a63b94a01de19b09
yuehong136 <1365590480@qq.com>
2024-10-16 13:41:49 +0800

    fix-keys of Xinference deployed models, especially has the same model name with public hosted models.

--- core/llm/sequence2txt_model/qwenseq2txt.py
+++ core/llm/sequence2txt_model/qwenseq2txt.py
@@ -9,7 +9,6 @@
 from core.llm.sequence2txt_model.base import Base
 from core.utils import num_tokens_from_string
 
-
 class QWenSeq2txt(Base):
     def __init__(self, key, model_name="paraformer-realtime-8k-v1", **kwargs):
         import dashscope
@@ -25,7 +24,7 @@ class QWenSeq2txt(Base):
                                   sample_rate=16000,
                                   callback=None)
         result = recognition.call(audio)
-
+        print(result)
         ans = ""
         if result.status_code == HTTPStatus.OK:
             for sentence in result.get_sentence():
@@ -34,3 +33,19 @@ class QWenSeq2txt(Base):
             return ans, num_tokens_from_string(ans)
 
         return "**ERROR**: " + result.message, 0
+
+if __name__ == "__main__":
+    # 设置API密钥和音频文件URL
+    api_key = "sk-4b492b0a99004a7da958b669858a2bdd"
+    audio_url = "test1.wav"  # 将此替换为有效的音频文件URL
+
+    # 初始化QWenSeq2txt类
+    model = QWenSeq2txt(api_key)
+
+    # 异步调用转写方法并获取结果
+    transcription, token_count = model.transcription(audio=audio_url)
+
+    # 输出转录结果
+    print("转录结果：")
+    print(transcription)
+    print("令牌数量：", token_count)
