4d25d358438194ef617f445ac87d8d83ec1e1375
yuehong136 <1365590480@qq.com>
2024-11-26 17:20:54 +0800

    Check model id when set dialog.

--- api/apps/dialog_app.py
+++ api/apps/dialog_app.py
@@ -142,6 +142,12 @@ async def set_dialog(request: DialogRequest, db: Session = Depends(get_db), user
         if not tenant:
             return get_data_error_result(retmsg="Tenant not found!")
 
+        kbs = KnowledgebaseService.get_by_ids(db, request.kb_ids)
+        embd_count = len(set([kb.embd_id for kb in kbs]))
+        if embd_count != 1:
+            return get_data_error_result(
+                retmsg=f'Datasets use different embedding models: {[kb.embd_id for kb in kbs]}"')
+
         llm_id = request.llm_id or tenant.llm_id
         if not request.dialog_id:
             dia = {
