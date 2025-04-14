08a7ec562eda23df741c283fa7b3e464b0a97926
yuehong136 <1365590480@qq.com>
2025-04-14 21:45:01 +0800

    Reformat error message.

--- api/apps/llm_app.py
+++ api/apps/llm_app.py
@@ -424,21 +424,22 @@ POST
     }
 
     msg = ""
+    mdl_nm = llm["llm_name"].split("___")[0]
     if llm["mdl_type"] == LLMType.EMBEDDING.value:
         mdl = EmbeddingModel[factory](
             key=llm['api_key'],
-            model_name=llm["llm_name"],
+            model_name=mdl_nm,
             base_url=llm["api_base"])
         try:
             arr, tc = mdl.encode(["Test if the api key is available"])
             if len(arr[0]) == 0:
                 raise Exception("Fail")
         except Exception as e:
-            msg += f"\nFail to access embedding model({llm['llm_name']})." + str(e)
+            msg += f"\nFail to access embedding model({mdl_nm})." + str(e)
     elif llm["mdl_type"] == LLMType.CHAT.value:
         mdl = ChatModel[factory](
             key=llm['api_key'],
-            model_name=llm["llm_name"],
+            model_name=mdl_nm,
             base_url=llm["api_base"]
         )
         try:
@@ -447,11 +448,11 @@ POST
             if m.find("**ERROR**") >= 0:
                 raise Exception(m)
         except Exception as e:
-            msg += f"\nFail to access model({llm['llm_name']})." + str(e)
+            msg += f"\nFail to access model({mdl_nm})." + str(e)
     elif llm["mdl_type"] == LLMType.RERANK:
         mdl = RerankModel[factory](
             key=llm["api_key"],
-            model_name=llm["llm_name"],
+            model_name=mdl_nm,
             base_url=llm["api_base"]
         )
         try:
@@ -459,12 +460,12 @@ POST
             if len(arr) == 0 or tc == 0:
                 raise Exception("Not known.")
         except Exception as e:
-            msg += f"\nFail to access model({llm['llm_name']})." + str(
+            msg += f"\nFail to access model({mdl_nm})." + str(
                 e)
     elif llm["mdl_type"] == LLMType.IMAGE2TEXT.value:
         mdl = CvModel[factory](
             key=llm["api_key"],
-            model_name=llm["llm_name"],
+            model_name=mdl_nm,
             base_url=llm["api_base"]
         )
         try:
@@ -476,13 +477,13 @@ POST
             msg += f"\nFail to access model({llm['llm_name']})." + str(e)
     elif llm["mdl_type"] == LLMType.TTS:
         mdl = TTSModel[factory](
-            key=llm["api_key"], model_name=llm["llm_name"], base_url=llm["api_base"]
+            key=llm["api_key"], model_name=mdl_nm, base_url=llm["api_base"]
         )
         try:
             for resp in mdl.tts("Hello~ Multirager!"):
                 pass
         except RuntimeError as e:
-            msg += f"\nFail to access model({llm['llm_name']})." + str(e)
+            msg += f"\nFail to access model({mdl_nm})." + str(e)
     else:
         # TODO: check other type of models
         pass
