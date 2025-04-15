ffb6d2d89425369903b99888be90c0639bc144f9
tyang <ytong@datav.com>
2025-04-15 09:53:39 +0800

    #12 实现数据库脚本热迁移

--- api/apps/conversation_app.py
+++ api/apps/conversation_app.py
@@ -633,8 +633,6 @@ async def delete_msg(request: DeleteMsgRequest, db: Session = Depends(get_db), u
         conv["message"].pop(i)
         if i < len(conv["message"]):
             conv["message"].pop(i)  # 因为前面 pop 了一次，后面的索引需要调整
-        # todo 待解决：目前无法完全符合预期删除reference（20240828）
-        # todo ragflow修复了上面的问题，待测试（20240830）
         if i < len(conv["reference"]):
             conv["reference"].pop(i)  # 同样对 reference 做相应的 pop 操作
         break
