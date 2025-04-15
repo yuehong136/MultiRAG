6b2fd957fc8f98c189e393bc9d0afc8df3f70dd5
yuehong136 <1365590480@qq.com>
2025-04-15 08:57:04 +0800

    Optimize conversation when uploading attachments

--- api/db/services/dialog_service.py
+++ api/db/services/dialog_service.py
@@ -236,9 +236,6 @@ def chat(dialog, messages, db: Session, stream=True, **kwargs):
     attachments = kwargs["doc_ids"].split(",") if "doc_ids" in kwargs else None
     if "doc_ids" in messages[-1]:
         attachments = messages[-1]["doc_ids"]
-        for m in messages[:-1]:
-            if "doc_ids" in m:
-                attachments.extend(m["doc_ids"])
 
     create_retriever_ts = timer()
 
