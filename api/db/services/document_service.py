f47709ec95c4453ab519753a54d938a0c548760a
yuehong136 <1365590480@qq.com>
2025-07-03 09:48:42 +0800

    fix: resolve residual image files issue after document deletion
    ### What problem does this PR solve?
    
    When deleting knowledge base documents in MultiRAG, the current process
    only removes the block texts in Elasticsearch and the original files in
    MinIO, but it leaves behind many binary images and thumbnails generated
    during chunking. This pull request improves the deletion process by
    querying the block information in Elasticsearch to ensure a more
    thorough and complete cleanup.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- api/db/services/document_service.py
+++ api/db/services/document_service.py
@@ -17,16 +17,18 @@ from pymilvus import MilvusException
 from sqlalchemy.exc import NoResultFound, OperationalError
 from sqlalchemy.orm import Session, aliased
 from sqlalchemy import func, asc, and_, or_
+
+from api.constants import IMG_BASE64_PREFIX
 from api.db import FileType, TaskStatus, StatusEnum, UserTenantRole
 from api.db.db_models import Document, Knowledgebase, Tenant, Task, UserTenant, db_connection
 from api.db.services.common_service import CommonService
 from api.db.services.knowledgebase_service import KnowledgebaseService
 from api.utils import current_timestamp, get_format_time, get_uuid
 from api.utils.db_utils import bulk_insert_into_db
-from api.settings import docStoreConn
+# from api.settings import docStoreConn
+from api import settings
 from core.settings import get_svr_queue_name
 from core.nlp import search, rag_tokenizer
-from core import settings
 from core.utils.storage_factory import STORAGE_IMPL
 from core.utils.redis_conn import REDIS_CONN
 from core.utils.doc_store_conn import OrderByExpr
@@ -214,31 +216,51 @@ class DocumentService(CommonService):
         # 在删除文档前先保存需要的属性
         doc_id = doc.id
         cls.clear_chunk_num(db, doc.id)
+
         document = DocumentService.get_by_doc_id(db, doc.id)
         kb = KnowledgebaseService.get_by_id(db, document["kb_id"])
         # 构建 Milvus 集合名称
         collection_name = search.index_name_one(tenant_id, kb.name)
-        # 检查集合是否存在并删除 Milvus 中的数据
+
+        page = 0
+        page_size = 1000
+        all_chunk_ids = []
+        while True:
+            chunks = settings.docStoreConn.search(["img_id"], [], {"doc_id": doc.id}, [], OrderByExpr(),
+                                                  page * page_size, page_size, collection_name,
+                                                  [doc.kb_id])
+            chunk_ids = settings.docStoreConn.getChunkIds(chunks)
+            if not chunk_ids:
+                break
+            all_chunk_ids.extend(chunk_ids)
+            page += 1
+        for cid in all_chunk_ids:
+            if STORAGE_IMPL.obj_exist(doc.kb_id, cid):
+                STORAGE_IMPL.rm(doc.kb_id, cid)
+        if doc.thumbnail and not doc.thumbnail.startswith(IMG_BASE64_PREFIX):
+            if STORAGE_IMPL.obj_exist(doc.kb_id, doc.thumbnail):
+                STORAGE_IMPL.rm(doc.kb_id, doc.thumbnail)
 
         try:
-            if docStoreConn.has_collection(collection_name):
-                docStoreConn.delete(
+            # 检查集合是否存在并删除 Milvus 中的数据
+            if settings.docStoreConn.has_collection(collection_name):
+                settings.docStoreConn.delete(
                     collection_name=collection_name,
                     filter=f"doc_id == '{doc_id}'"
                 )
-            # todo 待测试【docStoreConn.delete等】，测试成功则替换上面的方法 优先级较高，不然graphrag玩不转
+            # todo 待测试【settings.docStoreConn.delete等】，测试成功则替换上面的方法 优先级较高，不然graphrag玩不转
             # kb_id = document["kb_id"]  # 使用从数据库重新获取的kb_id
-            # graph_source = docStoreConn.getFields(
-            #     docStoreConn.search(["source_id"], [], {"kb_id": kb_id, "knowledge_graph_kwd": ["graph"]}, [], OrderByExpr(), 0, 1, search.index_name(tenant_id, [kb.name]), [kb_id]), ["source_id"]
+            # graph_source = settings.docStoreConn.getFields(
+            #     settings.docStoreConn.search(["source_id"], [], {"kb_id": kb_id, "knowledge_graph_kwd": ["graph"]}, [], OrderByExpr(), 0, 1, search.index_name(tenant_id, [kb.name]), [kb_id]), ["source_id"]
             # )
             # if len(graph_source) > 0 and doc_id in list(graph_source.values())[0]["source_id"]:
-            #     docStoreConn.update({"kb_id": kb_id, "knowledge_graph_kwd": ["entity", "relation", "graph", "subgraph", "community_report"], "source_id": doc_id},
+            #     settings.docStoreConn.update({"kb_id": kb_id, "knowledge_graph_kwd": ["entity", "relation", "graph", "subgraph", "community_report"], "source_id": doc_id},
             #                                 {"remove": {"source_id": doc_id}},
             #                                 search.index_name(tenant_id, [kb.name]), kb_id)
-            #     docStoreConn.update({"kb_id": kb_id, "knowledge_graph_kwd": ["graph"]},
+            #     settings.docStoreConn.update({"kb_id": kb_id, "knowledge_graph_kwd": ["graph"]},
             #                                 {"removed_kwd": "Y"},
             #                                 search.index_name(tenant_id, [kb.name]), kb_id)
-            #     docStoreConn.delete({"kb_id": kb_id, "knowledge_graph_kwd": ["entity", "relation", "graph", "subgraph", "community_report"], "must_not": {"exists": "source_id"}},
+            #     settings.docStoreConn.delete({"kb_id": kb_id, "knowledge_graph_kwd": ["entity", "relation", "graph", "subgraph", "community_report"], "must_not": {"exists": "source_id"}},
             #                                 search.index_name(tenant_id, [kb.name]), kb_id)
         except MilvusException as e:
             return e
