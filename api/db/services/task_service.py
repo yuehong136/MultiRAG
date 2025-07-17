5a7e8cae3a917fc6d1c862dd88060557ebed187b
yuehong136 <1365590480@qq.com>
2025-07-17 08:47:06 +0800

    fix task_service.py
    ### What problem does this PR solve?
    
    Fix the case where pages variable might be None
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- api/db/services/task_service.py
+++ api/db/services/task_service.py
@@ -310,6 +310,8 @@ def queue_tasks(db: Session, doc: dict, bucket: str, name: str, priority: int):
         file_bin = STORAGE_IMPL.get(bucket, name)
         do_layout = doc["parser_config"].get("layout_recognize", "DeepDOC")
         pages = PdfParser.total_page_number(doc["name"], file_bin)
+        if pages is None:
+            pages = 0
         page_size = doc["parser_config"].get("task_page_size") or 12
         if doc["parser_id"] == "paper":
             page_size = doc["parser_config"].get("task_page_size") or 22
