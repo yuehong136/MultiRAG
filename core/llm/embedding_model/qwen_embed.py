cc69869d43e86bcc92659ad3de11a1bf43546d7a
yuehong136 <1365590480@qq.com>
2025-07-17 08:49:04 +0800

    fix: retry embedding with Qwen family models when limits temporarily reached.
    APIs of Qwen family models are limited by calling rates. When reached,
    the "output" attribute of the "resp" will be None, and in turn cause
    TypeError when trying to retrieve "embeddings". Since these limits are
    almost temporary, I have added a simple retry mechanism to avoid it.
    Besides, if retry_max reached, the error can be early raised, instead of hidden behind "TypeError".
    
    ### What problem does this PR solve?
    
    Sometimes Qwen blocks calling due to rate limits, but it will cause the
    whole parsing procedure stops when creating knowledge base. In this
    situation, resp["output"] will be None, and resp["output"]["embeddings"]
    will cause TypeError. Since the limits are temporary, I apply a simple
    retry mechanism to solve it.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- core/llm/embedding_model/qwen_embed.py
+++ core/llm/embedding_model/qwen_embed.py
@@ -13,17 +13,22 @@ class QWenEmbed(Base):
 
     def encode(self, texts: list):
         import dashscope
+        import time
+
         batch_size = 4
         res = []
         token_count = 0
         texts = [truncate(t, 2048) for t in texts]
         for i in range(0, len(texts), batch_size):
-            resp = dashscope.TextEmbedding.call(
-                model=self.model_name,
-                input=texts[i:i + batch_size],
-                api_key=self.key,
-                text_type="document"
-            )
+            retry_max = 5
+            resp = dashscope.TextEmbedding.call(model=self.model_name, input=texts[i : i + batch_size], api_key=self.key, text_type="document")
+            while resp["output"] is None and retry_max > 0:
+                time.sleep(10)
+                resp = dashscope.TextEmbedding.call(model=self.model_name, input=texts[i : i + batch_size], api_key=self.key, text_type="document")
+                retry_max -= 1
+            if retry_max == 0 and resp["output"] is None:
+                log_exception(ValueError("Retry_max reached, calling embedding model failed"))
+                raise
             try:
                 embds = [[] for _ in range(len(resp["output"]["embeddings"]))]
                 for e in resp["output"]["embeddings"]:
@@ -36,14 +41,8 @@ class QWenEmbed(Base):
         return np.array(res), token_count
 
     def encode_queries(self, text):
-        resp = dashscope.TextEmbedding.call(
-            model=self.model_name,
-            input=text[:2048],
-            api_key=self.key,
-            text_type="query"
-        )
+        resp = dashscope.TextEmbedding.call(model=self.model_name, input=text[:2048], api_key=self.key, text_type="query")
         try:
-            return np.array(resp["output"]["embeddings"][0]
-                            ["embedding"]), self.total_token_count(resp)
+            return np.array(resp["output"]["embeddings"][0]["embedding"]), self.total_token_count(resp)
         except Exception as _e:
             log_exception(_e, resp)
