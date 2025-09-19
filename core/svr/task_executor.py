7401ef3fe3ecd13cd1759b30a743b33568c81d10
yuehong136 <1365590480@qq.com>
2025-09-19 10:15:15 +0800

    Fix broken data stream when writing image file
    ### What problem does this PR solve?
    
    fix "broken data stream when writing image file", just log warning and
    ignore
    
    Close #8379
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- core/svr/task_executor.py
+++ core/svr/task_executor.py
@@ -344,7 +344,11 @@ async def build_chunks(task, progress_callback, db: Session):
                         converted_image = d["image"].convert("RGB")
                         d["image"].close()  # Close original image
                         d["image"] = converted_image
-                    d["image"].save(output_buffer, format='JPEG')
+                    try:
+                        d["image"].save(output_buffer, format='JPEG')
+                    except OSError as e:
+                        logging.warning(
+                            "Saving image of chunk {}/{}/{} got exception, ignore: {}".format(task["location"], task["name"], d["id"], str(e)))
 
                 async with minio_limiter:
                     await trio.to_thread.run_sync(
