3a7e6bc1c070e9a7ec630fabed0bb85bf9311d17
yuehong136 <1365590480@qq.com>
2025-09-28 11:24:24 +0800

    Fix: import error

--- core/llm/cv.py
+++ core/llm/cv.py
@@ -25,7 +25,7 @@ from openai import OpenAI
 from openai.lib.azure import AzureOpenAI
 from zhipuai import ZhipuAI
 from core.nlp import is_english
-from core.prompts import vision_llm_describe_prompt
+from core.prompts.prompts import vision_llm_describe_prompt
 from core.utils import num_tokens_from_string
 
 
