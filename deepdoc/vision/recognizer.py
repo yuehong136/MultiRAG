39ebd619665a199083b76f2b5cc564cb487411d1
yuehong136 <1365590480@qq.com>
2025-10-24 15:47:49 +0800

    Fix: terminate onnx inference session manually
    ### What problem does this PR solve?
    
    terminate onnx inference session and release memory manually.
    
    Issue #5050
    Issue #9992
    Issue #8805
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- deepdoc/vision/recognizer.py
+++ deepdoc/vision/recognizer.py
@@ -10,7 +10,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-
+import gc
 import logging
 import os
 import math
@@ -403,6 +403,11 @@ class Recognizer:
             "score": float(scores[i])
         } for i in indices]
 
+    def close(self):
+        logging.info("Close recognizer.")
+        del self.ort_sess
+        gc.collect()
+
     def __call__(self, image_list, thr=0.7, batch_size=16):
         res = []
         images = []
@@ -427,5 +432,5 @@ class Recognizer:
 
         return res
 
-
-
+    def __del__(self):
+        self.close()
