43aa1eb56c3c82cd92eb695649c0d0a356dadc63
yuehong136 <1365590480@qq.com>
2025-11-03 17:09:07 +0800

    fix: decode before format to json
    ### What problem does this PR solve?
    
    Decode bytes before format to json.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- core/flow/parser/parser.py
+++ core/flow/parser/parser.py
@@ -414,7 +414,7 @@ class Parser(ProcessBase):
                         dispositions = content_disposition.strip().split(";")
                         if dispositions[0].lower() == "attachment":
                             filename = part.get_filename()
-                            payload = part.get_payload(decode=True)
+                            payload = part.get_payload(decode=True).decode(part.get_content_charset())
                             attachments.append({
                                 "filename": filename,
                                 "payload": payload,
@@ -451,7 +451,7 @@ class Parser(ProcessBase):
                 for t in msg.attachments:
                     attachments.append({
                         "filename": t.name,
-                        "payload": t.data  # binary
+                        "payload": t.data.decode("utf-8")
                     })
                 email_content["attachments"] = attachments
 
