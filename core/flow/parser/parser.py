b73445fc9d2788c974954683270651828f6e73d7
yuehong136 <1365590480@qq.com>
2025-11-05 15:28:05 +0800

    Fix: video file suffix
    ### What problem does this PR solve?
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- core/flow/parser/parser.py
+++ core/flow/parser/parser.py
@@ -145,7 +145,8 @@ class ParserParam(ProcessParamBase):
             "video": {
                 "suffix":[
                     "mp4",
-                    "av"
+                    "avi",
+                    "mkv"
                 ],
                 "output_format": "json",
             },
