dd8a4dd15b1a8fca4e1f886007d9d59eab53d1bf
yuehong136 <1365590480@qq.com>
2025-12-26 16:36:21 +0800

    Minor tweaks
    ### What problem does this PR solve?
    
    Minor tweaks
    
    ### Type of change
    
    - [x] Refactoring

--- agent/component/base.py
+++ agent/component/base.py
@@ -513,6 +513,7 @@ class ComponentBase(ABC):
     def get_param(self, name):
         if hasattr(self._param, name):
             return getattr(self._param, name)
+        return None
 
     def debug(self, **kwargs):
         return self._invoke(**kwargs)
@@ -520,7 +521,7 @@ class ComponentBase(ABC):
     def get_parent(self) -> Union[object, None]:
         pid = self._canvas.get_component(self._id).get("parent_id")
         if not pid:
-            return
+            return None
         return self._canvas.get_component(pid)["obj"]
 
     def get_upstream(self) -> List[str]:
@@ -545,7 +546,7 @@ class ComponentBase(ABC):
 
     def exception_handler(self):
         if not self._param.exception_method:
-            return
+            return None
         return {
             "goto": self._param.exception_goto,
             "default_value": self._param.exception_default_value
