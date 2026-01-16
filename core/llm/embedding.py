e9855753011a3b5a95431302cd4d1d86c404b933
yuehong136 <1365590480@qq.com>
2026-01-16 15:36:09 +0800

    fix: guard Dashscope response attribute access in token/log utils
    ### What problem does this PR solve?
    
    Guard Dashscope response attribute access in token/log utils, since
    `dashscope_response` returns dict like object.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- core/llm/embedding.py
+++ core/llm/embedding.py
@@ -248,7 +248,7 @@ class BaiChuanEmbed(OpenAIEmbed):
 class QWenEmbed(Base):
     _FACTORY_NAME = "Tongyi-Qianwen"
 
-    def __init__(self, key, model_name="text_embedding_v2", **kwargs):
+    def __init__(self, key, model_name="text-embedding-v4", **kwargs):
         self.key = key
         self.model_name = model_name
 
