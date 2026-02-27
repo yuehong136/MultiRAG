5719971e8f5dbe5e57e477b84f21daf14ca58d67
yuehong136 <1365590480@qq.com>
2026-02-27 16:14:40 +0800

    fix: add 'mom_id' column to OBConnection chunk table
    ### What problem does this PR solve?
    
    Fix #12428
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- core/utils/ob_conn.py
+++ core/utils/ob_conn.py
@@ -43,10 +43,11 @@ from common.doc_store.doc_store_base import DocStoreConnection, MatchExpr, Order
 ATTEMPT_TIME = 2
 OB_QUERY_TIMEOUT = int(os.environ.get("OB_QUERY_TIMEOUT", "100_000_000"))
 
-logger = logging.getLogger('ragflow.ob_conn')
+logger = logging.getLogger('multirag.ob_conn')
 
 column_order_id = Column("_order_id", Integer, nullable=True, comment="chunk order id for maintaining sequence")
 column_group_id = Column("group_id", String(256), nullable=True, comment="group id for external retrieval")
+column_mom_id = Column("mom_id", String(256), nullable=True, comment="parent chunk id")
 
 column_definitions: list[Column] = [
     Column("id", String(256), primary_key=True, comment="chunk id"),
@@ -91,6 +92,7 @@ column_definitions: list[Column] = [
     Column("extra", JSON, nullable=True, comment="extra information of non-general chunk"),
     column_order_id,
     column_group_id,
+    column_mom_id,
 ]
 
 column_names: list[str] = [col.name for col in column_definitions]
@@ -537,7 +539,7 @@ class OBConnection(DocStoreConnection):
                 column_name = fts_column.split("^")[0]
                 if not self._index_exists(table_name, fulltext_index_name_template % column_name):
                     return False
-            for column in [column_order_id, column_group_id]:
+            for column in [column_order_id, column_group_id, column_mom_id]:
                 if not self._column_exist(table_name, column.name):
                     return False
         except Exception as e:
@@ -591,7 +593,7 @@ class OBConnection(DocStoreConnection):
             )
 
             # new columns migration
-            for column in [column_order_id, column_group_id]:
+            for column in [column_order_id, column_group_id, column_mom_id]:
                 _try_with_lock(
                     lock_name=f"ob_add_{column.name}_{indexName}",
                     check_func=lambda: self._column_exist(indexName, column.name),
