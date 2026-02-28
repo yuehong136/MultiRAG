39e5d6a0b11cd054a9e8cca69257c838932d09d8
yuehong136 <1365590480@qq.com>
2026-02-28 10:51:49 +0800

    fix(alembic): rename system_settings setting_type column to source

---
+++ configs/alembic/versions/d3c94b4c95c9_update_columns.py
@@ -0,0 +1,59 @@
+"""update columns
+
+Revision ID: d3c94b4c95c9
+Revises: 95fb64826988
+Create Date: 2026-02-28 10:48:40.112481
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import Inspector
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'd3c94b4c95c9'
+down_revision: Union[str, None] = '95fb64826988'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    bind = op.get_bind()
+    insp = Inspector.from_engine(bind)
+
+    tables = insp.get_table_names(schema="usr_ai")
+    if "t_ai_system_settings" not in tables:
+        return
+
+    cols = [col["name"] for col in insp.get_columns("t_ai_system_settings", schema="usr_ai")]
+    if "setting_type" in cols and "source" not in cols:
+        with op.batch_alter_table("t_ai_system_settings", schema="usr_ai") as batch_op:
+            batch_op.alter_column(
+                "setting_type",
+                new_column_name="source",
+                existing_type=sa.String(length=32),
+                existing_nullable=False,
+            )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    bind = op.get_bind()
+    insp = Inspector.from_engine(bind)
+
+    tables = insp.get_table_names(schema="usr_ai")
+    if "t_ai_system_settings" not in tables:
+        return
+
+    cols = [col["name"] for col in insp.get_columns("t_ai_system_settings", schema="usr_ai")]
+    if "source" in cols and "setting_type" not in cols:
+        with op.batch_alter_table("t_ai_system_settings", schema="usr_ai") as batch_op:
+            batch_op.alter_column(
+                "source",
+                new_column_name="setting_type",
+                existing_type=sa.String(length=32),
+                existing_nullable=False,
+            )
