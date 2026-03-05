e89532c9d363aa6dc2813c24c2f63761eba011d8
yuehong136 <1365590480@qq.com>
2026-03-05 18:00:21 +0800

    fix(entrypoint): Preserve $ in passwords during template expansion
    ### What problem does this PR solve?
    
    Fix shell variable expansion to preserve $ in password defaults when
    env vars are unset. Fixes Azure RDS auto-rotated passwords (that contain
    $) being
    truncated during template processing.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -170,8 +170,18 @@ generate_config() {
   # 从模板生成配置文件
   echo "[entrypoint] 从模板生成配置文件: ${TEMPLATE_FILE} -> ${CONF_FILE}"
   rm -f "${CONF_FILE}"
+  DEF_ENV_VALUE_PATTERN="\$\{([^:]+):-([^}]+)\}"
   while IFS= read -r line || [[ -n "$line" ]]; do
-    eval "echo \"$line\"" >> "${CONF_FILE}"
+    if [[ "$line" =~ DEF_ENV_VALUE_PATTERN ]]; then
+      varname="${BASH_REMATCH[1]}"
+      if [ -n "${!varname}" ]; then
+        eval "echo \"$line\"" >> "${CONF_FILE}"
+      else
+        echo "$line" | sed -E "s/\\\$\{[^:]+:-([^}]+)\}/\1/g" >> "${CONF_FILE}"
+      fi
+    else
+      eval "echo \"$line\"" >> "${CONF_FILE}"
+    fi
   done < "${TEMPLATE_FILE}"
   echo "[entrypoint] 配置文件生成完成"
 }
