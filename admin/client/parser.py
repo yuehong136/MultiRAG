6948726604641e28d2f77817894ff995d05c2179
yuehong136 <1365590480@qq.com>
2026-06-08 16:10:25 +0800

    Fix: allow on
    ### What problem does this PR solve?
    
    Allow input on/ON as status.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- admin/client/parser.py
+++ admin/client/parser.py
@@ -263,7 +263,7 @@ action_list: identifier ("," identifier)*
 
 identifier: WORD
 quoted_string: QUOTED_STRING
-status: WORD
+status: ON | WORD
 
 QUOTED_STRING: /'[^']+'/ | /"[^"]+"/
 WORD: /[a-zA-Z0-9_\-\.]+/
